c165b2056f82e5412fb6e48ff6caa637dd40146e
brist <bristonmekus@gamil.com>
2017-03-04 22:18:49 +0100

    implement edit feature & attempting to add local strorage ability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,11 +19,30 @@ export class AppComponent {
 
 
   // Initiating the Index and Id variable for the tasks
-  id = 0;
+  id:number = 0;
+  // Initiating the Task object in the createTask function
+  task:any;
+  // Initiating the Index variable in the deleteTask function
+  index:number;
+  // Initiating the indexOfTask variable in the editTask function
+  indexOfTask:number;
+  // 
+  editting:boolean = false;
+
 
   // Initiating class constructor
   constructor() {  
-	  this.tasks = []
+	  this.tasks = [];
+    console.log(this.tasks);
+    sessionStorage.setItem("storedTasks",JSON.stringify(this.tasks));
+    let storedTask = sessionStorage.getItem("storedTasks");
+    console.log(storedTask + " this is session storage");
+    storedTask = JSON.parse(storedTask);
+    console.log(JSON.parse(storedTask) + " parsed to Json");
+    // console.log(localStorage.length);
+
+    this.tasks.push(storedTask);  
+    console.log(storedTask + " newly stored");
   }
 
   // Function to create new task
@@ -35,37 +54,39 @@ export class AppComponent {
         // incrementing task Id for each created task by 1
         this.id ++;
         // Initiating task object
-        let task = {
+        this.task = {
           "id": this.id,
           "title": title,
           "description": description   
         }  
         // Storing new instance task object in local storage per session
-   
-        this.tasks.push(task);  
-        console.log(this.tasks);
 
-        // localStorage.setItem("storedTasks",JSON.stringify(this.tasks));
-        // let storedTask = localStorage.getItem("storedTasks");
-        // console.log(localStorage.getItem("storedTasks"));
-        // console.log(storedTask);
-        // this.tasks = JSON.parse(storedTask);
-        // console.log(JSON.parse(storedTask) + " parsed to Json");
-        // console.log(localStorage.length);
+        
       }
     }
 
   // Function to delete task
   deleteTask(taskOne) {
-    // Getting the index of an existing task from the table
-    let index = this.tasks.findIndex(task => task.id === taskOne.id);
-    console.log(index);
+    // Getting the index of an existing task in the table
+    this.index = this.tasks.findIndex(task => task.id === taskOne.id);
+    console.log(this.index);
       // Deleting existing task form the table
-      this.tasks.splice(index, 1);
+      this.tasks.splice(this.index, 1);
       // Alerting user of before deleting
       alert("Task deleted!");
       console.log(this.tasks);      
   }
 
+  // Function to edit task
+  editTask(editableTask) {
+    this.editting = true;
+    this.indexOfTask = this.tasks.indexOf(editableTask);
+  }
+
+  // Display edited task in console
+  consoleThis() {
+    console.log(this.tasks[this.indexOfTask].title);
+  }
 }
+
 
